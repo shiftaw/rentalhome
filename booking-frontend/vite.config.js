f354cbe9477e5b98eb8bd9316b7f8e79ceae203e
shiftaw <shiftaw.shiftaw@gmail.com>
2025-05-01 23:35:18 +0200

    fix server

--- booking-frontend/vite.config.js
+++ booking-frontend/vite.config.js
@@ -5,14 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-    }
+
 
   },
 
